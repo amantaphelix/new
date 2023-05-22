390b6e4a14cfc9ef998e29d971ee4a1124143696
amantaphelix <95036405+amantaphelix@users.noreply.github.com>
2023-05-22 16:00:07 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,29 @@
+function time(a){
+    if(a<=11){
+        console.log("good morning");
+    }else if(a>=13 && a<=15){
+        console.log("good afternoon");
+    }else if(a>=16 && a<=18){
+        console.log("good evening");
+    }else{
+        console.log("good night");
+    }
+}
+time(9);
+
+function oddoreven(b){
+    if(b%2==0){
+        console.log("Even")
+    }else{
+        console.log("Odd")
+    }
+}
+oddoreven(2);
+
+function time2(c){
+    if(c.includes(AM)){
+        console.log(c);
+    }else{
+        
+    }
+}
